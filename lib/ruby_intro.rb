7a92a90c2dbc4ffdba426cb7e08adc67df9b84cb
Mazin Hakeem <mazhak@msn.com>
2015-08-30 09:15:05 +0000

    Solved everything except for the last 2 in part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,35 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.inject(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.size == 1
+  #arr.sort[-2..-1].inject(:+)
+  arr.sort.last(2).inject(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty? or arr.size == 1
+  a = arr.combination(2).to_a
+  a.each { |x| return true if x.inject(:+) == n }
+  return false
+  
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  st = /[aeiou]/ =~ s.downcase.chars.first
+  return true if st == nil
+  return false
 end
 
 def binary_multiple_of_4? s
@@ -31,5 +41,15 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? or price < 1
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    "$%.2f" % price
+  end
 end
